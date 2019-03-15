209642f6aa8985895b611f97fe2a6a36f463ea5c
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-15 10:05:43 +0100

    Create viral_assemblies.py

---
+++ viral_assemblies.py
@@ -0,0 +1,15 @@
+from Bio import Entrez, SeqIO
+import pandas as pd
+
+Entrez.email = 'massimo.bourquin@epfl.ch'
+
+data_inputs = []
+with open('assembly_virus_complete_genomes.txt', 'r') as f:
+    for line in f.readlines():
+        data_inputs.append(line.replace('\n',''))
+
+with open('28350_viral_genomes.fasta', 'w') as f:
+    for genome_id in data_inputs:
+        record = Entrez.efetch(db='nucleotide', id=genome_id, rettype='fasta', retmode='text')
+        filename = 'generated/genBankRecord_{}.gb'.format(genome_id)
+        f.write(record.read())
